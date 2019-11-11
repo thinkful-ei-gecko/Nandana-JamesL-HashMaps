6624de424ac3514db5e20cadb3b26068e5e9a6ad
nrrao <nandanarao@yahoo.com>
2019-11-11 16:34:05 -0500

    started chaining

--- hashmapDrills.js
+++ hashmapDrills.js
@@ -16,7 +16,7 @@ const WhatDoesThisDo = function(){
 
   console.log(map1.get(str1));
   console.log(map2.get(str3));
-}
+};
 
 //#3 Hash map understanding
 //1. k mod m: 10 % 11 = 1, 22 % 11 = 0, 31 % 11 = 9, 4 % 11 = 7, 15 % 11 = 4, 28 % 11 = 6, 17 % 11 = 6
@@ -29,36 +29,114 @@ const WhatDoesThisDo = function(){
 //input: google
 //output: gole 
 function removeDupe(str) {
-  let newStr = ''
-  let hash = new HashMap()
+  // let arr = str.split('');
+  // console.log(arr)
+  let result='';
+  let hash = new HashMap();
   for(let i = 0; i < str.length; i++) {
-    const index = hash._findSlot(str[i])
-    hash.set(str[i], 1)
-    newStr += hash._hashTable[index].key
+    
+    if(!hash.get(str[i])){
+      hash.set(str[i],true);
+      result += str[i];
+    }
   }
-  console.log(newStr)
-  // return newStr
+  return result;
 }
+
+function palindrome(s) {
+  let oddCount = 0;
+  let hash = new HashMap();
+  for(let i=0; i<s.length; i++) {
+    if(hash.get(s[i])) {
+      hash.set(s[i], hash.get(s[i]) + 1);
+      if(hash.get(s[i]) % 2 === 1){
+        oddCount++;
+      }else {
+        oddCount--;
+      }
+    }
+    else {
+      hash.set(s[i], 1);
+      ++oddCount;
+    }
+  }
+  return oddCount === 1 || oddCount === 0;
+}
+
+// function anagram(arr){
+//   const arrHash = new HashMap();
+//   arr.forEach(word => {
+//     let keyValue = word.split('').sort().join();
+//     if(arrHash.get(keyValue) === undefined){
+//       arrHash.set(keyValue,[word]);
+//     } else{
+//       let temp = arrHash.get(keyValue);
+//       arrHash.set(keyValue,[...word,temp]);
+
+//     }
+//   });
+//   return arrHash.get();
+// }
+
+function groupAnagrams(wordList) {
+  const hashMap = new HashMap();
+  let groupsArray = []; //Groups of anagrams will be pushed here
+
+  const groupIndexFinder = (array, grouping) => {
+    //Loop through groupsArray and find the grouping(e.s aest)
+    for (let i = 0; i < array.length; i++) {
+      if (
+        array[i][0]
+          .split("")
+          .sort()
+          .join("") === grouping
+      ) {
+        //return the index position of array
+        return i;
+      }
+    }
+  };
+
+  for (let i = 0; i < wordList.length; i++) {
+    const grouping = wordList[i]
+      .split("")
+      .sort()
+      .join("");
+    if (hashMap.get(grouping) === undefined) {
+      groupsArray.push([wordList[i]]);
+      hashMap.set(grouping, groupIndexFinder(groupsArray, grouping));
+    } else {
+      groupsArray[hashMap.get(grouping)].push(wordList[i]);
+    }
+  }
+
+  return groupsArray;
+}
+
+let arr = ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']
+console.log(groupAnagrams(arr))
 
 function main() {
-  const lor = new HashMap()
-  lor.set('Hobbit', 'Bilbo')
-  lor.set('Hobbit', 'Frodo')
-  lor.set('Wizard', 'Gandolf')
-  lor.set('Human', 'Aragon')
-  lor.set('Elf', 'Legolas')
-  lor.set('Maiar', 'Necromancer')
-  lor.set('Maiar', 'Sauron')
-  lor.set('RingBearer', 'Gollum')
-  lor.set('LadyOfLight', 'Galadriel')
-  lor.set('HalfElven', 'Arwen')
-  lor.set('Ent', 'Treebeard')
+  const lor = new HashMap();
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
   // console.log(lor._findSlot('Hobbit'))
-  console.log(lor._hashTable[13].key)
+  console.log(lor._hashTable[13].key);
 }
 
-HashMap.MAX_LOAD_RATIO = 0.5
-HashMap.SIZE_RATIO = 3
+HashMap.MAX_LOAD_RATIO = 0.5;
+HashMap.SIZE_RATIO = 3;
 
-console.log(removeDupe('google'))
-
+// console.log(removeDupe('google'));
+// console.log(palindrome('acecarr'));
+// console.log(palindrome('north'));
+console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
